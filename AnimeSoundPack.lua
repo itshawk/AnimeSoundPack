182f9c709af29abcff82f516b6e1ff37c12b8a79
BeeVa <itshawk@gnode.org>
2021-08-03 07:17:34 -0700

    Initial Commit: Priest and Shaman Healie bois

---
+++ AnimeSoundPack.lua
@@ -0,0 +1,114 @@
+local ADDON = ...
+local PATH = "Interface\\AddOns\\AnimeSoundPack\\SoundFiles\\"
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- Priest
+LSM:Register("sound", "Priest Spells: Pain", [[Interface\Addons\AnimeSoundPack\priest_meme\weirdpain.ogg]])
+LSM:Register("sound", "Priest Spells: Shadow Word Pain", [[Interface\Addons\AnimeSoundPack\priest_meme\shadowwordpain.ogg]])
+
+-- Holy
+LSM:Register("sound", "Holy Priest Spells: Apotheosis (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\Apotheosis_(1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Apotheosis (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\Apotheosis_(2).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Apotheosis (3)", [[Interface\Addons\AnimeSoundPack\priest_meme\Apotheosis_(3).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Circle Of Healing (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\Circle of Healing_(1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Circle Of Healing (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\Circle of Healing_(2).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Circle Of Healing (3)", [[Interface\Addons\AnimeSoundPack\priest_meme\Circle of Healing_(3).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Divine Star (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\Divine_Star_(1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Divine Star (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\Divine Star_(2).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Divine Hymn (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\Divine Hymn (1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Divine Hymn (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\Divine Hymn (2).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Divine Star (3)", [[Interface\Addons\AnimeSoundPack\priest_meme\Divine Star_(3).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Halo (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\Halo_(1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Halo (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\Halo_(2).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Heal (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\Heal_(1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Heal (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\Heal_(2).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Holy Fire (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\Holy Fire_(1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Holy Fire (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\Holy Fire_(2).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Holy Fire (3)", [[Interface\Addons\AnimeSoundPack\priest_meme\Holy Fire_(3).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Ineffable Truth (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\Ineffable Truth_(1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Ineffable Truth (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\Ineffable Truth_(2).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Prayer of Mending (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\PoM_(1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Prayer of Mending (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\PoM_(2).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Renew (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\Renew_(1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Renew (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\Renew_(2).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Salvation (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\Salvation_(1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Salvation (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\Salvation_(2).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Sanctify (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\Sanctify_(1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Sanctify (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\Sanctify_(2).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Serenity (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\Serenity_(1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Serenity (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\Serenity_(2).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Serenity (3)", [[Interface\Addons\AnimeSoundPack\priest_meme\Serenity_(3).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Use (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\Use_(1).ogg]])
+LSM:Register("sound", "Holy Priest Spells: Use (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\Use_(2.ogg]])
+LSM:Register("sound", "Holy Priest Spells: Use (3)", [[Interface\Addons\AnimeSoundPack\priest_meme\Use_(3).ogg]])
+
+-- Disc
+LSM:Register("sound", "Disc Priest Spells: Mindblast (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\mindblast.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Mindblast (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\mindblast2.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Barrier (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\barrier.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Barrier (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\barrier2.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Power Word Barrier (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\powerwordbarrier.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Power Word Barrier (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\powerwordbarrier2.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Power Word Radiance", [[Interface\Addons\AnimeSoundPack\priest_meme\powerwordradiance.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Radiance (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\radiance.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Radiance (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\radiance2.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Power Word Radiance", [[Interface\Addons\AnimeSoundPack\priest_meme\PowerWordRadiance.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Purge the Wicked", [[Interface\Addons\AnimeSoundPack\priest_meme\purgethewicked.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Rapture", [[Interface\Addons\AnimeSoundPack\priest_meme\rapture.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Shadowfiend", [[Interface\Addons\AnimeSoundPack\priest_meme\shadowfiend.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Schism", [[Interface\Addons\AnimeSoundPack\priest_meme\shism.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Solace (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\solace.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Solace (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\solace2.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Spirit Shell", [[Interface\Addons\AnimeSoundPack\priest_meme\spiritshell.ogg]])
+LSM:Register("sound", "Disc Priest Spells: MindBender", [[Interface\Addons\AnimeSoundPack\priest_meme\mindbender.ogg]])
+LSM:Register("sound", "Disc Priest Spells: MindSear", [[Interface\Addons\AnimeSoundPack\priest_meme\mindsear.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Pain Sup", [[Interface\Addons\AnimeSoundPack\priest_meme\painsup.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Penance", [[Interface\Addons\AnimeSoundPack\priest_meme\penance.ogg]])
+LSM:Register("sound", "Disc Priest Spells: Evangelism (scuffed lol)", [[Interface\Addons\AnimeSoundPack\priest_meme\eveangleism.ogg]])
+
+-- Shaman
+LSM:Register("sound", "Shaman Spells: Unleash", [[Interface\Addons\AnimeSoundPack\priest_meme\unleash.ogg]])
+LSM:Register("sound", "Shaman Spells: Unleash Life", [[Interface\Addons\AnimeSoundPack\priest_meme\unleashlife.ogg]])
+LSM:Register("sound", "Shaman Spells: PW", [[Interface\Addons\AnimeSoundPack\priest_meme\pw.ogg]])
+LSM:Register("sound", "Shaman Spells: Primordial Wave", [[Interface\Addons\AnimeSoundPack\priest_meme\PrimordialWave.ogg]])
+LSM:Register("sound", "Shaman Spells: Riptide (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\riptide.ogg]])
+LSM:Register("sound", "Shaman Spells: Riptide (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\riptide2.ogg]])
+LSM:Register("sound", "Shaman Spells: SLT", [[Interface\Addons\AnimeSoundPack\priest_meme\slt.ogg]])
+LSM:Register("sound", "Shaman Spells: Sprit Link Totem", [[Interface\Addons\AnimeSoundPack\priest_meme\SpiritLinkTotem.ogg]])
+LSM:Register("sound", "Shaman Spells: Sprit Link", [[Interface\Addons\AnimeSoundPack\priest_meme\SpiritLink.ogg]])
+LSM:Register("sound", "Shaman Spells: Healing Rain (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\HealingRain.ogg]])
+LSM:Register("sound", "Shaman Spells: Healing Rain (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\HealingRain2.ogg]])
+LSM:Register("sound", "Shaman Spells: Healing Tide Totem", [[Interface\Addons\AnimeSoundPack\priest_meme\HealingTideTotem.ogg]])
+LSM:Register("sound", "Shaman Spells: HTT", [[Interface\Addons\AnimeSoundPack\priest_meme\htt.ogg]])
+LSM:Register("sound", "Shaman Spells: Mana Tide Totem", [[Interface\Addons\AnimeSoundPack\priest_meme\ManaTideTotem.ogg]])
+LSM:Register("sound", "Shaman Spells: MTT", [[Interface\Addons\AnimeSoundPack\priest_meme\MTT.ogg]])
+LSM:Register("sound", "Shaman Spells: Ancestral Protection Totem", [[Interface\Addons\AnimeSoundPack\priest_meme\AncestralProtectionTotem.ogg]])
+LSM:Register("sound", "Shaman Spells: APT", [[Interface\Addons\AnimeSoundPack\priest_meme\apt.ogg]])
+LSM:Register("sound", "Shaman Spells: Cloud", [[Interface\Addons\AnimeSoundPack\priest_meme\cloud.ogg]])
+LSM:Register("sound", "Shaman Spells: Cloudburst", [[Interface\Addons\AnimeSoundPack\priest_meme\cloudburst.ogg]])
+LSM:Register("sound", "Shaman Spells: Earthen Wall", [[Interface\Addons\AnimeSoundPack\priest_meme\earthenwall.ogg]])
+
+-- Misc
+LSM:Register("sound", "Misc Spells: Trinket (1)", [[Interface\Addons\AnimeSoundPack\priest_meme\trinket.ogg]])
+LSM:Register("sound", "Misc Spells: Trinket (2)", [[Interface\Addons\AnimeSoundPack\priest_meme\trinket2.ogg]])
+LSM:Register("sound", "Misc Spells: Start Ramp", [[Interface\Addons\AnimeSoundPack\priest_meme\startramp.ogg]])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
